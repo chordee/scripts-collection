964bab3b6b59c6746d3b8a52be23666c157196f5
chordee <chordee@gmail.com>
2020-05-20 10:58:55 +0800

    rv_run

--- rv_run.py
+++ rv_run.py
@@ -1,18 +1,21 @@
-import os
+import os, sys
 
 PROGRAM_FILES = 'C:/Program Files'
-RV_VER = '0.0'
+os.environ['RV_SUPPORT_PATH'] = 'Q:\\Resource\\rv\\plugins'
 
-
-def main():
+def main(dark = False):
     try:
         if 'Shotgun' in os.listdir(PROGRAM_FILES):
             rv_dirs = os.listdir('/'.join([PROGRAM_FILES, 'Shotgun']))
             if len(rv_dirs) > 0:
+                rv_dirs.sort()
                 rv_run = '/'.join([PROGRAM_FILES, 'Shotgun', rv_dirs[-1], 'bin', 'rv.exe'])
                 if os.path.exists(rv_run) is True:
-                    RV_VER = rv_dirs[-1][3:]
-                    os.system('"' + rv_run.replace('/', '\\') + '"')
+                    cmd = '"' + rv_run.replace('/', '\\') + '"'
+                    if dark is False:
+                        os.system(cmd)
+                    else:
+                        os.system(cmd + ' -qtstyle fusion -qtcss Q:\\Resource\\rv\\darkorange.qss')
                 else:
                     raise EnvironmentError('Execute not Found.')
             else:
@@ -23,4 +26,8 @@ def main():
         print(err)
 
 if __name__ == "__main__":
-    main()
+    if len(sys.argv) > 1:
+        if sys.argv[1] == '-dark':
+            main(dark = True)
+    else:
+        main()
