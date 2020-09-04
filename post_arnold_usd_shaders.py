b25eba2455ddc82b36c8f9e78247f5f4fc1e348d
chordee <chordee@gmail.com>
2020-09-04 20:12:22 +0800

    Add files via upload

---
+++ post_arnold_usd_shaders.py
@@ -0,0 +1,80 @@
+import maya.cmds as cmds
+import maya.api.OpenMaya as om
+from pxr import Usd, UsdShade, UsdGeom, Sdf
+
+filename = "D:/tmp/arnold.usda"
+
+
+class MtoaShadersToUSD:
+
+    def __init__(self, filename, root):
+        self.filename = filename
+        self.root = root
+        self.shadingGroups = getShadingGroups(root)
+
+    def exportUSD(self, scope="/materials"):
+        self.scope = scope
+        cmds.select(self.root, r=1)
+        cmds.arnoldExportAss(f=self.filename, s=1, shadowLinks=0, mask=16,
+                             lightLinks=0, forceTranslateShadingEngines=1, boundingBox=1, fullPath=1)
+        self.post_peocess()
+
+    def post_peocess(self):
+        stage = Usd.Stage.Open(self.filename)
+        edit = Sdf.BatchNamespaceEdit()
+
+        UsdGeom.Scope.Define(stage, self.scope)
+        UsdShade.Material.Define(stage, self.scope + "/test")
+
+        for prim in stage.Traverse():
+            if prim.GetTypeName() == "Shader":
+                path = prim.GetPath()
+                edit.Add(path, '/materials/test' + str(path))
+                shader = UsdShade.Shader.Define(stage, prim.GetPath())
+                for i in shader.GetInputs():
+                    if i.HasConnectedSource() is True:
+                        print(i.GetFullName())
+
+        stage.GetRootLayer().Apply(edit)
+
+        edit_dict = {}
+        for i in edit.edits:
+            edit_dict[i.currentPath] = i.newPath
+
+        for prim in stage.Traverse():
+            if prim.GetTypeName() == "Shader":
+                shader = UsdShade.Shader.Define(stage, prim.GetPath())
+                for i in shader.GetInputs():
+                    attr = i.GetAttr()
+                    if len(attr.GetConnections()) > 0:
+                        con = attr.GetConnections()[0]
+                        print con.GetPrimPath() in edit_dict.keys()
+                        k = con.ReplacePrefix(
+                            con.GetPrimPath(), edit_dict[con.GetPrimPath()])
+                        i.ConnectToSource(k)
+
+        print(stage.ExportToString())
+
+
+def getShadingGroups(root):
+    children_meshs = cmds.listRelatives(
+        root, ad=True, typ='surfaceShape', f=True)
+    mesh_list = om.MSelectionList()
+    for mesh in children_meshs:
+        mesh_list.add(mesh)
+    shadingGroup_list = []
+    for i in range(mesh_list.length()):
+        mesh = om.MFnMesh(mesh_list.getDagPath(i))
+        shadingGroups = [om.MFnDependencyNode(
+            x).name() for x in mesh.getConnectedShaders(0)[0]]
+        shadingGroup_list += shadingGroups
+    shadingGroup_list = list(set(shadingGroup_list))
+
+    return shadingGroup_list
+
+
+if __name__ == "__main__":
+    sel = cmds.ls(sl=True)[0]
+
+    k = MtoaShadersToUSD(filename, sel)
+    k.exportUSD()
