0c9c661a3dee589263140c34c330594980c13a93
Chordee <chordee@gmail.com>
2022-05-18 19:08:26 +0800

    export materials assignment USD

--- maya/usd/USD.py
+++ maya/usd/USD.py
@@ -1,6 +1,8 @@
 
 from PySide2 import QtWidgets, QtGui, QtCore
 from maya import cmds
+from pxr import Usd, UsdGeom, UsdShade
+import maya.api.OpenMaya as om
 
 
 class USD_Tab(QtWidgets.QWidget):
@@ -19,11 +21,18 @@ class USD_Tab(QtWidgets.QWidget):
             'Create SkelRoot Attribute')
         self.crate_usd_preview_shader_btn = QtWidgets.QPushButton(
             'Build USD Preview Sahder')
+        self.export_materials_assignment_btn = QtWidgets.QPushButton(
+            "Export Materials Assignment")
         self.buttons_layout.addWidget(self.create_skelroot_btn)
         self.buttons_layout.addWidget(self.crate_usd_preview_shader_btn)
+        self.buttons_layout.addWidget(self.export_materials_assignment_btn)
+
         self.create_skelroot_btn.clicked.connect(self.create_skelroot_action)
         self.crate_usd_preview_shader_btn.clicked.connect(
             self.create_usd_preview_shader)
+        self.export_materials_assignment_btn.clicked.connect(
+            self.export_materials_assugnment)
+
         self.layout.setAlignment(QtCore.Qt.AlignTop)
         self.layout.addStretch()
 
@@ -38,6 +47,71 @@ class USD_Tab(QtWidgets.QWidget):
     def create_usd_preview_shader(self):
         dialog = Build_USD_Preview_Shader(self)
         dialog.show()
+
+    def export_materials_assugnment(self, merge=False, scopeName='Looks', purpose='all', assetVersion=None, assetName=None):
+        dagObject = cmds.ls(sl=True)[0]
+
+        if not dagObject:
+            return
+
+        shapeChildren = cmds.listRelatives(
+            dagObject, ad=True, f=True, typ='shape')
+
+        omList = om.MSelectionList()
+        for shape in shapeChildren:
+            omList.add(shape)
+
+        stage = Usd.Stage.CreateInMemory()
+
+        meshLsit = [om.MFnMesh(omList.getDagPath(i))
+                    for i in range(omList.length())]
+
+        for i, mesh in zip(range(omList.length()), meshLsit):
+            path = omList.getDagPath(i).fullPathName()
+            shaders, indices = mesh.getConnectedShaders(0)
+            prim = stage.OverridePrim(path.replace('|', '/'))
+            root = stage.GetPrimAtPath('/').GetAllChildren()[0]
+            scope = stage.OverridePrim(root.GetPath().AppendChild(scopeName))
+            if len(shaders) == 1:
+                shadingGroup_name = om.MFnDependencyNode(shaders[0]).name()
+                material_name = cmds.listConnections(
+                    shadingGroup_name + '.surfaceShader')[0]
+                # change from shadingGroup name to material name
+                shadingGroup_name = material_name
+                usdMaterial = UsdShade.Material.Define(
+                    stage, scope.GetPrim().GetPath().AppendChild(shadingGroup_name))
+                UsdShade.MaterialBindingAPI(prim).Bind(usdMaterial)
+            elif len(shaders) > 1:
+                shadingGroup_names = [
+                    om.MFnDependencyNode(x).name() for x in shaders]
+                shader_index = 0
+                for shadingGroup_name in shadingGroup_names:
+                    material_name = cmds.listConnections(
+                        shadingGroup_name + '.surfaceShader')[0]
+                    # change from shadingGroup name to material name
+                    shadingGroup_name = material_name
+                    geomSubset = UsdGeom.Subset.Define(
+                        stage, prim.GetPrim().GetPath().AppendChild(shadingGroup_name))
+                    geomSubset.CreateElementTypeAttr('face')
+                    geomSubset.CreateIndicesAttr(
+                        [i for i, x in enumerate(indices) if x == shader_index])
+                    usdMaterial = UsdShade.Material.Define(
+                        stage, scope.GetPrim().GetPath().AppendChild(shadingGroup_name))
+                    UsdShade.MaterialBindingAPI(geomSubset).Bind(
+                        usdMaterial, materialPurpose=purpose)
+                    shader_index += 1
+
+        rootPrim = stage.GetPrimAtPath('/').GetAllChildren()[0]
+        if assetVersion:
+            rootPrim.SetAssetInfoByKey('version', assetVersion)
+        if assetName:
+            rootPrim.SetAssetInfoByKey('name', assetName)
+        stage.SetDefaultPrim(rootPrim)
+
+        filenames = cmds.fileDialog2(
+            fm=0, startingDirectory='C:/', fileFilter="USD (*.usd *.usda)")
+        filename = filenames[0]
+        stage.Export(filename)
 
 
 class Build_USD_Preview_Shader(QtWidgets.QDialog):
