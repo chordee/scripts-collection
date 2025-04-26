b4f1fc549df7eaae9cfe3759b83e8064e53b17a5
chordee <chordee@gmail.com>
2025-04-27 01:06:54 +0800

    mod: Code refinement

--- CPP/separate_exr.cpp
+++ CPP/separate_exr.cpp
@@ -1,153 +1,222 @@
 #include <iostream>
 #include <string>
 #include <vector>
-#include <typeinfo>
 #include <filesystem>
 #include <map>
+#include <algorithm>
 
 #include <OpenImageIO/imageio.h>
 #include <OpenImageIO/imagebuf.h>
 #include <OpenImageIO/imagebufalgo.h>
 
 using namespace OIIO;
-using namespace std;
-using namespace ImageBufAlgo;
 
-vector<string> split(const string &str, string delim){
-    vector<string> result;
-    string::size_type pos = str.find(delim);
-    result.push_back(str.substr(0, pos));
-    result.push_back(str.substr(pos+1));
+std::vector<std::string> split(const std::string &str, const std::string &delim)
+{
+    std::vector<std::string> result;
+    size_t start = 0, end;
+    while ((end = str.find(delim, start)) != std::string::npos)
+    {
+        result.push_back(str.substr(start, end - start));
+        start = end + delim.length();
+    }
+    result.push_back(str.substr(start));
     return result;
-};
+}
 
-void separate_exr(const string &filename, bool extBasename = 0){
-        ImageBuf &in = ImageBuf(filename);
-        filesystem::path &path = filesystem::path(filename);
-        const filesystem::path &source_file = path.filename();
-        const auto source_names = split(source_file.string(), ".");
+void separate_exr(const std::string &filename, bool extBasename = false)
+{
+    OIIO::ImageBuf in(filename);
+    std::filesystem::path path(filename);
+    const std::filesystem::path source_file = path.filename();
+    const auto source_names = split(source_file.string(), ".");
 
-        if(!in.read())
-            return;
+    if (!in.read())
+    {
+        std::cerr << "Failed to read: " << filename << std::endl;
+        return;
+    }
 
-        const ImageSpec &spec = in.spec();
+    const OIIO::ImageSpec &spec = in.spec();
 
-        int xres = spec.width;
-        int yres = spec.height;
-        TypeDesc format = spec.format;
-        int nchannels = spec.nchannels;
-        int pixels = xres * yres;
-        map<string, vector<int>> channel_map;
+    int xres = spec.width;
+    int yres = spec.height;
+    OIIO::TypeDesc format = spec.format;
+    int nchannels = spec.nchannels;
+    int pixels = xres * yres;
+    std::map<std::string, std::vector<int>> channel_map;
 
-        for(int i = 0; i < nchannels; ++i){
-            const string &ch_name = spec.channelnames[i];
-            if(ch_name.size() > 0){
-                auto splitted = split(ch_name, ".");
+    for (int i = 0; i < nchannels; ++i)
+    {
+        const std::string &ch_name = spec.channelnames[i];
+        if (!ch_name.empty())
+        {
+            auto splitted = split(ch_name, ".");
+            if (!splitted.empty())
+            {
                 auto iter = channel_map.find(splitted[0]);
-                if(iter == channel_map.end()){
-                    channel_map.insert(pair<string, vector<int>>(splitted[0], {i}));
+                if (iter == channel_map.end())
+                {
+                    channel_map.insert({splitted[0], {i}});
                 }
-                else{
-                    channel_map[splitted[0]].push_back(i);
+                else
+                {
+                    iter->second.push_back(i);
                 }
             }
         }
-        auto p0 = find(spec.channelnames.begin(), spec.channelnames.end(), "R");
-        auto p1 = find(spec.channelnames.begin(), spec.channelnames.end(), "G");
-        auto p2 = find(spec.channelnames.begin(), spec.channelnames.end(), "B");
-        auto p3 = find(spec.channelnames.begin(), spec.channelnames.end(), "A");
-        if(p3 == spec.channelnames.end()){ 
-            vector<int> tmp = {(int)distance(spec.channelnames.begin(), p0), (int)distance(spec.channelnames.begin(), p1), (int)distance(spec.channelnames.begin(), p2)};
-            channel_map["beauty"] = tmp;
-        }else{
-            vector<int> tmp = {(int)distance(spec.channelnames.begin(), p0), (int)distance(spec.channelnames.begin(), p1), (int)distance(spec.channelnames.begin(), p2), (int)distance(spec.channelnames.begin(), p3)};
-            channel_map["beauty"] = tmp;
+    }
+
+    auto p0 = std::find(spec.channelnames.begin(), spec.channelnames.end(), "R");
+    auto p1 = std::find(spec.channelnames.begin(), spec.channelnames.end(), "G");
+    auto p2 = std::find(spec.channelnames.begin(), spec.channelnames.end(), "B");
+    auto p3 = std::find(spec.channelnames.begin(), spec.channelnames.end(), "A");
+    if (p0 != spec.channelnames.end() && p1 != spec.channelnames.end() && p2 != spec.channelnames.end())
+    {
+        std::vector<int> tmp = {
+            static_cast<int>(std::distance(spec.channelnames.begin(), p0)),
+            static_cast<int>(std::distance(spec.channelnames.begin(), p1)),
+            static_cast<int>(std::distance(spec.channelnames.begin(), p2))};
+        if (p3 != spec.channelnames.end())
+        {
+            tmp.push_back(static_cast<int>(std::distance(spec.channelnames.begin(), p3)));
         }
-        for(auto &it : channel_map){
-            if(it.second.size() > 1){
-                auto dir = path.parent_path().append(it.first);
-                filesystem::create_directory(dir);
-                filesystem::path dst;
-                if(extBasename == 0){
-                    dst = dir.append(source_names[0] + "." + source_names[1]);
-                }else{
-                    dst = dir.append(source_names[0] + "_" + it.first + "." + source_names[1]);
-                }
-                ImageBuf out;
-                int *order;
-                if(it.second.size() == 3){
-                    if(it.first == "N"){
-                        auto p0 = find(spec.channelnames.begin(), spec.channelnames.end(), "N.x");
-                        auto p1 = find(spec.channelnames.begin(), spec.channelnames.end(), "N.y");
-                        auto p2 = find(spec.channelnames.begin(), spec.channelnames.end(), "N.z");
-                        int tmp[] = {(int)distance(spec.channelnames.begin(), p0), (int)distance(spec.channelnames.begin(), p1), (int)distance(spec.channelnames.begin(), p2)};
-                        order = &tmp[0];
-                    }else{
-                        order = &(it.second[0]);
+        channel_map["beauty"] = tmp;
+    }
+
+    for (auto &it : channel_map)
+    {
+        if (it.second.size() > 1)
+        {
+            auto dir = path.parent_path() / it.first;
+            std::filesystem::create_directories(dir);
+            std::filesystem::path dst;
+            if (source_names.size() < 2)
+            {
+                std::cerr << "Invalid filename format: " << source_file << std::endl;
+                continue;
+            }
+            if (!extBasename)
+            {
+                dst = dir / (source_names[0] + "." + source_names[1]);
+            }
+            else
+            {
+                dst = dir / (source_names[0] + "_" + it.first + "." + source_names[1]);
+            }
+            OIIO::ImageBuf out;
+            std::vector<int> order;
+            if (it.second.size() == 3)
+            {
+                if (it.first == "N")
+                {
+                    auto np0 = std::find(spec.channelnames.begin(), spec.channelnames.end(), "N.x");
+                    auto np1 = std::find(spec.channelnames.begin(), spec.channelnames.end(), "N.y");
+                    auto np2 = std::find(spec.channelnames.begin(), spec.channelnames.end(), "N.z");
+                    if (np0 != spec.channelnames.end() && np1 != spec.channelnames.end() && np2 != spec.channelnames.end())
+                    {
+                        order = {
+                            static_cast<int>(std::distance(spec.channelnames.begin(), np0)),
+                            static_cast<int>(std::distance(spec.channelnames.begin(), np1)),
+                            static_cast<int>(std::distance(spec.channelnames.begin(), np2))};
                     }
-                    string names[] = {"R", "G", "B"};
-                    float values[] = {0, 0, 0};
-                    ImageBufAlgo::channels(out, in, 3, order, values, names);
-                    out.write(dst.string());
-                }else if(it.second.size() == 4){
-                    order = &(it.second[0]);
-                    string names[] = {"R", "G", "B", "A"};
-                    float values[] = {0, 0, 0, 0};
-                    ImageBufAlgo::channels(out, in, 4, order, values, names);
-                    out.write(dst.string());
+                    else
+                    {
+                        order = it.second;
+                    }
                 }
+                else
+                {
+                    order = it.second;
+                }
+                const char *names[] = {"R", "G", "B"};
+                float values[] = {0, 0, 0};
+                OIIO::ImageBufAlgo::channels(out, in, 3, order.data(), values, names);
+                out.write(dst.string());
+            }
+            else if (it.second.size() == 4)
+            {
+                order = it.second;
+                const char *names[] = {"R", "G", "B", "A"};
+                float values[] = {0, 0, 0, 0};
+                OIIO::ImageBufAlgo::channels(out, in, 4, order.data(), values, names);
+                out.write(dst.string());
             }
         }
-        auto z = find(spec.channelnames.begin(), spec.channelnames.end(), "Z");
-        if(z == spec.channelnames.end()){
+    }
+
+    auto z = std::find(spec.channelnames.begin(), spec.channelnames.end(), "Z");
+    if (z == spec.channelnames.end())
+    {
+        return;
+    }
+    else
+    {
+        int z_channel[] = {static_cast<int>(std::distance(spec.channelnames.begin(), z))};
+        float values[] = {0};
+        const char *names[] = {"Z"};
+
+        OIIO::ImageBuf out;
+        OIIO::ImageBufAlgo::channels(out, in, 1, z_channel, values, names);
+
+        auto dir = path.parent_path() / "depth";
+        std::filesystem::create_directories(dir);
+        std::filesystem::path dst;
+
+        if (source_names.size() < 2)
+        {
+            std::cerr << "Invalid filename format: " << source_file << std::endl;
             return;
-        }else{
-            int z_channel[] = {spec.z_channel};     
-            float values[] = {0};
-            string names[] = {"Z"};
-
-            ImageBuf out;
-            ImageBufAlgo::channels(out, in, 1, z_channel, values, names);
-
-            auto dir = path.parent_path().append("depth");
-            filesystem::create_directory(dir);
-            filesystem::path dst;
-
-            
-            if(extBasename == 0){
-                dst = dir.append(source_names[0] + "." + source_names[1]);
-            }else{
-                dst = dir.append(source_names[0] + "_depth." + source_names[1]);
-            }
-            bool ok = out.write(dst.string());
         }
-};
+        if (!extBasename)
+        {
+            dst = dir / (source_names[0] + "." + source_names[1]);
+        }
+        else
+        {
+            dst = dir / (source_names[0] + "_depth." + source_names[1]);
+        }
+        bool ok = out.write(dst.string());
+        if (!ok)
+        {
+            std::cerr << "Failed to write depth image: " << dst << std::endl;
+        }
+    }
+}
 
-int main(int argc, char **argv){
-    vector<string> v_argv(argv, argv + argc);
-    
-    int extbase = 0;
-    auto ptr = find(v_argv.begin(), v_argv.end(), "-extBase");
+int main(int argc, char **argv)
+{
+    std::vector<std::string> v_argv(argv, argv + argc);
 
-    if(ptr != v_argv.end())
-        extbase = 1;
+    bool extbase = false;
+    auto ptr = std::find(v_argv.begin(), v_argv.end(), "-extBase");
 
-    for(int i = 1; i < argc; i++){
-        filesystem::path path(v_argv[i]);
-        if(filesystem::exists(path)){
-            if(!filesystem::is_directory(path)){
-                cout << path.extension() << endl;
-                if(path.extension() == ".exr"){
-                    separate_exr(filesystem::absolute(path).string(), extbase);
+    if (ptr != v_argv.end())
+        extbase = true;
+
+    for (int i = 1; i < argc; i++)
+    {
+        std::filesystem::path path(v_argv[i]);
+        if (std::filesystem::exists(path))
+        {
+            if (!std::filesystem::is_directory(path))
+            {
+                std::cout << path.extension() << std::endl;
+                if (path.extension() == ".exr")
+                {
+                    separate_exr(std::filesystem::absolute(path).string(), extbase);
                 }
-            }else{
-                for(auto &it : filesystem::directory_iterator(path)){
-                    if(!filesystem::is_directory(it.path()) && it.path().extension() == ".exr"){
-                        separate_exr(filesystem::absolute(it.path()).string(), extbase);
+            }
+            else
+            {
+                for (auto &it : std::filesystem::directory_iterator(path))
+                {
+                    if (!std::filesystem::is_directory(it.path()) && it.path().extension() == ".exr")
+                    {
+                        separate_exr(std::filesystem::absolute(it.path()).string(), extbase);
                     }
                 }
             }
-            
         }
     }
     return 0;
--- HOUDINI/fit_deadline_scheduler.py
+++ HOUDINI/fit_deadline_scheduler.py
@@ -7,6 +7,69 @@ startupinfo = None
 creationflags = 0x08000000
 
 
+def parseOutput(output):
+    """
+    Decodes the given output bytes and splits it into a list of strings by Windows line endings.
+
+    Args:
+        output (bytes): The output from a subprocess, expected as bytes.
+
+    Returns:
+        list: List of strings, each representing a line from the decoded output.
+    """
+    result = output.decode().split("\r\n")
+    return result
+
+
+def insertEnv(parm, env):
+    """
+    Inserts a new environment variable entry into a Houdini multiparm parameter.
+
+    Args:
+        parm: The multiparm parameter to insert the environment variable into.
+        env (str): The name of the environment variable to insert.
+
+    Side Effects:
+        Modifies the multiparm parameter by adding a new instance and setting its value.
+    """
+    parm.insertMultiParmInstance(0)
+    parm.multiParmInstances()[0].set(env)
+    parm.multiParmInstances()[1].set(os.environ[env].replace("\\", "/"))
+
+
+def setEnv(parm, envs):
+    """
+    Sets environment variable values for a Houdini multiparm parameter, adding new entries as needed.
+
+    Args:
+        parm: The multiparm parameter containing environment variable entries.
+        envs (list): List of environment variable names to set.
+
+    Side Effects:
+        Updates the multiparm parameter with the values of the specified environment variables.
+        Adds new entries for any environment variables not already present.
+    """
+    instances = parm.multiParmInstances()
+    envname_parms = list()
+    env_val_parms = list()
+    for i in instances:
+        if "envname" in i.name():
+            envname_parms.append(i)
+        else:
+            env_val_parms.append(i)
+
+    for idx, i in enumerate(envname_parms):
+        env_name = i.eval()
+        if i.eval() in envs:
+            env_val_parms[idx].set(os.environ[env_name].replace("\\", "/"))
+            index = envs.index(env_name)
+            envs.pop(index)
+
+    if len(envs) > 0:
+        for i in envs:
+            insertEnv(parm, i)
+
+
 class Dialog(QtWidgets.QWidget):
     def __init__(self, node, parent=None):
         QtWidgets.QWidget.__init__(self, parent)
@@ -16,16 +79,16 @@ class Dialog(QtWidgets.QWidget):
     def initUI(self):
         self.groups_combo = QtWidgets.QComboBox()
         self.pools_combo = QtWidgets.QComboBox()
-        self.set_btn = QtWidgets.QPushButton('Set')
+        self.set_btn = QtWidgets.QPushButton("Set")
 
         main_v_layout = QtWidgets.QVBoxLayout()
         pools_h_layout = QtWidgets.QHBoxLayout()
         groups_h_layout = QtWidgets.QHBoxLayout()
 
-        pools_h_layout.addWidget(QtWidgets.QLabel('Pools:'))
+        pools_h_layout.addWidget(QtWidgets.QLabel("Pools:"))
         pools_h_layout.addWidget(self.pools_combo)
 
-        groups_h_layout.addWidget(QtWidgets.QLabel('Groups:'))
+        groups_h_layout.addWidget(QtWidgets.QLabel("Groups:"))
         groups_h_layout.addWidget(self.groups_combo)
 
         main_v_layout.addWidget(QtWidgets.QLabel(node.path()))
@@ -37,10 +100,10 @@ class Dialog(QtWidgets.QWidget):
 
         self.set_btn.clicked.connect(self.auto_fit)
 
-    def setComboBoxItems(self, items, combo_type='groups'):
-        if combo_type == 'groups':
+    def setComboBoxItems(self, items, combo_type="groups"):
+        if combo_type == "groups":
             combo = self.groups_combo
-        elif combo_type == 'pools':
+        elif combo_type == "pools":
             combo = self.pools_combo
         else:
             return
@@ -51,105 +114,76 @@ class Dialog(QtWidgets.QWidget):
     def auto_fit(self):
         group = self.groups_combo.currentText()
         pool = self.pools_combo.currentText()
-        node.parm('deadline_jobgroup').set(group)
-        node.parm('deadline_jobpool').set(pool)
-        node.parm('deadline_mqjobgroup').set(group)
-        node.parm('deadline_mqjobpool').set(pool)
+        node.parm("deadline_jobgroup").set(group)
+        node.parm("deadline_jobpool").set(pool)
+        node.parm("deadline_mqjobgroup").set(group)
+        node.parm("deadline_mqjobpool").set(pool)
         # node.parm('mqusage').set(0)
-        node.parm('deadline_mqjobpriority').set(80)
-        node.parm('pdg_rpctimeout').set(10)
-        node.parm('pdg_rpcretries').set(5)
-        if hou.applicationVersion()[0]==20 or hou.applicationVersion()[0]==19:
-            node.parm('deadline_overrideplugin').set(1)
-            if hou.applicationVersion()[0]==20 and hou.applicationVersion()[1]==5:
-                node.parm('deadline_plugindirectory').set('Q:/Resource/H205_PDGDeadline')
+        node.parm("deadline_mqjobpriority").set(80)
+        node.parm("pdg_rpctimeout").set(10)
+        node.parm("pdg_rpcretries").set(5)
+        if hou.applicationVersion()[0] == 20 or hou.applicationVersion()[0] == 19:
+            node.parm("deadline_overrideplugin").set(1)
+            if hou.applicationVersion()[0] == 20 and hou.applicationVersion()[1] == 5:
+                node.parm("deadline_plugindirectory").set(
+                    "Q:/Resource/H205_PDGDeadline"
+                )
             else:
-                node.parm('deadline_plugindirectory').set('Q:/Resource/H20_PDGDeadline')
-            node.parm('deadline_copyplugin').set(0)
-            if node.parm('deadline_cmdinheritlocalenv'):
-                node.parm('deadline_cmdinheritlocalenv').set(1)
+                node.parm("deadline_plugindirectory").set("Q:/Resource/H20_PDGDeadline")
+            node.parm("deadline_copyplugin").set(0)
+            if node.parm("deadline_cmdinheritlocalenv"):
+                node.parm("deadline_cmdinheritlocalenv").set(1)
 
-            
         self.close()
 
 
-def parseOutput(output):
-    result = output.decode().split('\r\n')
-    return result
-
-
-def setCombBoxItems(combo, items):
-    for ind, item in enumerate(items):
-        combo.insertItem(ind, item)
-
-
-def setEnv(parm, envs):
-    instances = parm.multiParmInstances()
-    envname_parms = list()
-    env_val_parms = list()
-    for i in instances:
-        if 'envname' in i.name():
-            envname_parms.append(i)
-        else:
-            env_val_parms.append(i)
-
-    for idx, i in enumerate(envname_parms):
-        env_name = i.eval()
-        if i.eval() in envs:
-            env_val_parms[idx].set(os.environ[env_name].replace('\\', '/'))
-            index = envs.index(env_name)
-            envs.pop(index)
-
-    if len(envs) > 0:
-        for i in envs:
-            insertEnv(parm, i)
-
-
-def insertEnv(parm, env):
-    parm.insertMultiParmInstance(0)
-    parm.multiParmInstances()[0].set(env)
-    parm.multiParmInstances()[1].set(os.environ[env].replace('\\', '/'))
-
-
-
-
 node = hou.selectedNodes()[0]
-if node.type().name() == 'deadlinescheduler':
-    q_hfs = 'Q:/Resource/hfs-' + hou.applicationVersionString()
-    hfs = 'C:/Program Files/Side Effects Software/Houdini ' + \
-        hou.applicationVersionString()
-    exe = 'hython\\$PDG_EXE'
-    if os.path.exists(q_hfs):
-        node.parm('deadline_hfs').set(q_hfs)
-    else:
-        node.parm('deadline_hfs').set(hfs)
-    node.parm('deadline_python').set(exe)
-    node.parm('deadline_inheritlocalenv').set(1)
-    node.parm('deadline_ignoreexitcode').set(0)
-    node.parm('deadline_verboselog').set(1)
-    node.parm('deadline_jobpriority').set(80)
-    node.parm('deadline_repository').set('//deadline/DeadlineRepository10')
-    
-    # env_parm = node.parm('deadline_envmulti')
-    # alt_envs = ['HOUDINI_PATH', 'PATH', 'HOUDINI_DSO_ERROR', 'PXR_PLUGINPATH_NAME']
-    # env_names = [x for x in alt_envs if x in os.environ.keys()]
-    # setEnv(env_parm, env_names)
 
-    deadlineBin = os.environ['DEADLINE_PATH']
+if node.type().name() == "deadlinescheduler":
+    q_hfs = "Q:/Resource/hfs-" + hou.applicationVersionString()
+    hfs = (
+        "C:/Program Files/Side Effects Software/Houdini "
+        + hou.applicationVersionString()
+    )
+    exe = "hython\\$PDG_EXE"
+    if os.path.exists(q_hfs):
+        node.parm("deadline_hfs").set(q_hfs)
+    else:
+        node.parm("deadline_hfs").set(hfs)
+    node.parm("deadline_python").set(exe)
+    node.parm("deadline_inheritlocalenv").set(1)
+    node.parm("deadline_ignoreexitcode").set(0)
+    node.parm("deadline_verboselog").set(1)
+    node.parm("deadline_jobpriority").set(80)
+    node.parm("deadline_repository").set("//deadline/DeadlineRepository10")
+
+    deadlineBin = os.environ["DEADLINE_PATH"]
 
     deadlineCommand = os.path.join(deadlineBin, "deadlinecommand.exe -Pools")
-    proc = subprocess.Popen(deadlineCommand, stdin=subprocess.PIPE, stdout=subprocess.PIPE,
-                            stderr=subprocess.PIPE, shell=False, creationflags=creationflags)
+    proc = subprocess.Popen(
+        deadlineCommand,
+        stdin=subprocess.PIPE,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+        shell=False,
+        creationflags=creationflags,
+    )
     output, err = proc.communicate()
     pools = parseOutput(output)
 
     deadlineCommand = os.path.join(deadlineBin, "deadlinecommand.exe -Groups")
-    proc = subprocess.Popen(deadlineCommand, stdin=subprocess.PIPE, stdout=subprocess.PIPE,
-                            stderr=subprocess.PIPE, shell=False, creationflags=creationflags)
+    proc = subprocess.Popen(
+        deadlineCommand,
+        stdin=subprocess.PIPE,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+        shell=False,
+        creationflags=creationflags,
+    )
     output, err = proc.communicate()
     groups = parseOutput(output)
 
     dialog = Dialog(node=node)
-    dialog.setComboBoxItems(groups, combo_type='groups')
-    dialog.setComboBoxItems(pools, combo_type='pools')
+    dialog.setComboBoxItems(groups, combo_type="groups")
+    dialog.setComboBoxItems(pools, combo_type="pools")
     dialog.show()
