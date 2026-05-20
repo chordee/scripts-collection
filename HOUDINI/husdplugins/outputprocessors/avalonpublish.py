943d4897309376544b914d319d1accabcb74603c
chordee <chordee@gmail.com>
2026-05-20 16:07:15 +0800

    refactor: clean up avalonpublish output processor and add README
    - Remove unreachable branch in processReferencePath
    - Generalize Python procedural bypass (any .py asset path)
    - Fix processSavePath indentation
    - Use getpass.getuser() instead of os.getlogin() for headless safety
    - Make path prefix check case-insensitive with separator boundary
    - Drop misleading asset_is_layer comment
    - Add Chinese README documenting installation and API

--- HOUDINI/husdplugins/outputprocessors/avalonpublish.py
+++ HOUDINI/husdplugins/outputprocessors/avalonpublish.py
@@ -1,4 +1,5 @@
 import os
+import getpass
 import time
 
 import hou
@@ -8,7 +9,7 @@ import husd.outputprocessor as base
 class AvalonPublish(base.OutputProcessor):
 
     def __init__(self):
-        super(AvalonPublish, self).__init__()
+        super().__init__()
         self.output_dir = None
 
     @staticmethod
@@ -20,52 +21,36 @@ class AvalonPublish(base.OutputProcessor):
         return 'Avalon Publish'
 
     def beginSave(self, config_node, config_overrides, lop_node, t, stage_variables):
-
-        super(AvalonPublish, self).beginSave(
-            config_node, config_overrides, lop_node, t, stage_variables)
-
+        super().beginSave(config_node, config_overrides, lop_node, t, stage_variables)
         self.output_usd = self.evalConfig('lopoutput', config_node, config_overrides, t)
-        (self.output_dir, filename) = os.path.split(self.output_usd)
+        self.output_dir, _ = os.path.split(self.output_usd)
 
-    def processReferencePath(
-            self, asset_path, referencing_layer_path, asset_is_layer):
-
-        # If the asset_path is in one of the search directories, generate the
-        # asset_path as a search path. Don't use search paths for save paths,
-        # which must be returned as full paths. Also, search paths only work
-        # on layer files, so ignore other asset types.
-
-        if asset_path == 'invokegraph.py':
-            # Houdini Ocean Procedural -- Chordee
+    def processReferencePath(self, asset_path, referencing_layer_path, asset_is_layer):
+        # Python procedurals are resolved by the USD plugin, not as file paths.
+        if asset_path.endswith('.py'):
             return asset_path
 
-        abs_asset_path = hou.text.abspath(asset_path, self.output_dir)
-        abs_asset_path = hou.text.normpath(abs_asset_path)
-
         if not self.output_dir:
+            return asset_path
+
+        abs_asset_path = hou.text.normpath(hou.text.abspath(asset_path, self.output_dir))
+        output_dir_norm = self.output_dir.rstrip('/').rstrip('\\') + '/'
+
+        if not abs_asset_path.lower().startswith(output_dir_norm.lower()):
             return abs_asset_path
 
-        if not abs_asset_path.startswith(self.output_dir):
-            return abs_asset_path
-        if abs_asset_path.startswith(self.output_dir):
-            rel_path = hou.text.relpath(abs_asset_path, referencing_layer_path)
-            return rel_path
-        return asset_path
+        return hou.text.relpath(abs_asset_path, referencing_layer_path)
 
-    def processSavePath(
-            self, asset_path, referencing_layer_path, asset_is_layer):
-
-        # Treat asset paths as being relative to the output path.
+    def processSavePath(self, asset_path, referencing_layer_path, asset_is_layer):
         if self.output_dir:
-           return hou.text.abspath(asset_path, self.output_dir)
-
+            return hou.text.abspath(asset_path, self.output_dir)
         return asset_path
 
     def processLayer(self, layer):
         layer_data = layer.customLayerData
         layer_data['hip_file'] = hou.hipFile.path()
         layer_data['create_time'] = time.ctime()
-        layer_data['user'] = os.getlogin()
+        layer_data['user'] = getpass.getuser()
         layer.customLayerData = layer_data
         return True
 
